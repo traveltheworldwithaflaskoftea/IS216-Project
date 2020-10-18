14135702901f646ada08cbeabdb7f3793846eed7
nrald <72239345+nrald@users.noreply.github.com>
2020-10-18 17:26:38 +0800

    individual page

--- project/ownjs/4individualpage.js
+++ project/ownjs/4individualpage.js
@@ -1,24 +1,25 @@
+function search_dog_id(pet_id){
+    console.log("pet id");
+	var id = '7xl0fwJI98JMdZBn8vxztbMrxm7sIgf31I7wRBchOGkAOWeGnO';
+	var secret = 'pSAX1F4ihX08DiHz6uH9AGOWvzVRTZTgAQfbta0E';
+
+	var client  = new petfinder.Client({apiKey: id, secret: secret});
+
+    console.log(typeof(pet_id));
+
+	client.animal.show(pet_id)
+	.then(resp => {
+		// Do something with resp.data.breeds
+		var results_array = resp.data.animal;
+		console.log(results_array);
+    });
+    console.log('done running');
+}
 function load_individual_dog(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    console.log(id); 
-    search_dog_id(id);
+    const pet_id = urlParams.get('id')
+    console.log(pet_id); 
+    search_dog_id(pet_id);
 
-}
-function search_dog_id(id){
-	var id = '7xl0fwJI98JMdZBn8vxztbMrxm7sIgf31I7wRBchOGkAOWeGnO';
-	var secret = 'pSAX1F4ihX08DiHz6uH9AGOWvzVRTZTgAQfbta0E';
-	var client  = new petfinder.Client({apiKey: id, secret: secret});
-
-	client.animal.search({
-		type: 'dog',
-		id = id
-		})
-	.then(resp => {
-		// Do something with resp.data.breeds
-		var results_array = resp.data.animals;
-		console.log(results_array);
-		//
-	});
 }
