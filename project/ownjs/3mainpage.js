7e8c9a158028f98b97c8a814e0b4e2dc678f5614
nrald <72239345+nrald@users.noreply.github.com>
2020-10-23 18:54:34 +0800

    Update 3mainpage.js

--- project/ownjs/3mainpage.js
+++ project/ownjs/3mainpage.js
@@ -3,7 +3,6 @@ cartIDArray = [];
 var us_states={"AL":"Alabama","AK":"Alaska","AS":"American Samoa","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","DC":"District Of Columbia","FM":"Federated States Of Micronesia","FL":"Florida","GA":"Georgia","GU":"Guam","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MH":"Marshall Islands","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","MP":"Northern Mariana Islands","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PW":"Palau","PA":"Pennsylvania","PR":"Puerto Rico","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VI":"Virgin Islands","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
 
 function list_states(){
-    console.log("hello");
     var list_html='';
     for (var[key,value] of Object.entries(us_states))
     {
@@ -21,7 +20,6 @@ function list_dog() {
             for (dog of dog_dataset){
                 dog_list_html+=`<li><a href="#myInput" onclick="onclick_breed('${dog.name}')">${dog.name}</a></li>`;
             }
-            console.log(dog_list_html);
             document.getElementById("insert_list1").innerHTML=dog_list_html;
         }
     }
