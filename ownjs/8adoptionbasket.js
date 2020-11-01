1e401abe77078871a5ab63f76ef156320822e66f
nrald <72239345+nrald@users.noreply.github.com>
2020-11-01 22:22:34 +0800

    pet display

--- ownjs/8adoptionbasket.js
+++ ownjs/8adoptionbasket.js
@@ -46,22 +46,8 @@ function run_sql(){
 
 let pet_list = [];
 
-function addPets(){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const pet_id = urlParams.get('id')
-    check=pet_list.includes(pet_list)
-    if (check==false){
-        pet_list.push(pet_id);
-    }
-    console.log(pet_list);
-}
-
-
 
 function adoptionBasket(){
-    console.log("hi");
-    console.log(pet_list);
     for (pet in pet_list){
         var id = '7xl0fwJI98JMdZBn8vxztbMrxm7sIgf31I7wRBchOGkAOWeGnO';
         var secret = 'pSAX1F4ihX08DiHz6uH9AGOWvzVRTZTgAQfbta0E';
@@ -83,21 +69,20 @@ function adoptionBasket(){
 function display_adoption_basket_cards(result_array){
     var html = ''; // This will be used to replace dog-card-deck innerHTML later
 		for (result of result_array){
-			if(result.photos.length > 0){ //making sure the array has pictures
-				html += `<div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
-							<div class="trainer">
-							<figure>
-							<a href='4individualpage.html?id=${result.id}'><img src="${result.photos[0]['full']}" alt="Image" class="img-fluid"></a>
-							</figure>
-							<div class="px-md-3">
-								<h3>${result.name}</h3>
-								<p>${result.description}</p>
-							</div>
-							</div>
-						</div>
-						`;
+                html += `<div id="Display_Adoption_Basket" class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
+                  <div class="trainer">
+                    <figure>
+                    <img src="${result.photos[0]['full']}" alt="Image" class="img-fluid">
+                    </figure>
+                    <div class="px-md-3">
+                      <h3>${result.name}</h3>
+                      <p>${result.description}</p>
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="selected_pets[]" value="${result.id}">
+                        <label class="form-check-label" for="exampleCheck1">Add to Cart</label>
+                    </div>
+                  </div>
+              </div>`;
 				}
-			}
 		// end for loop, now lets put html inside the dog-card-deck
-		document.getElementById('wishlist').innerHTML = html;
+		document.getElementById('Display_Adoption_Basket').innerHTML = html;
 }
