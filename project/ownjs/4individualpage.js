01022ddc3c408779903e49dbb5ab195d2d5f088d
nrald <72239345+nrald@users.noreply.github.com>
2020-11-01 16:05:57 +0800

    add to basket

--- project/ownjs/4individualpage.js
+++ project/ownjs/4individualpage.js
@@ -71,8 +71,11 @@ function display_individual_page(dog_array){
         document.getElementById('caro_buttons_left').innerHTML = '';
         document.getElementById('caro_buttons_right').innerHTML = '';
     }
+<<<<<<< Updated upstream
     //time to update some values!!
     
+=======
+>>>>>>> Stashed changes
 
     document.getElementById('dog_description').innerHTML = `
                                                         <p>${dog_array.description}</p>
@@ -117,5 +120,6 @@ function display_individual_page(dog_array){
                                                 </ul>
                                                 `;                                                                                                   
 }
+
 
 
