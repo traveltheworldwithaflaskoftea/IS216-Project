ac75f58be03fbcb67d285974275b593330162d09
kingsoncjx <72540450+kingsoncjx@users.noreply.github.com>
2020-11-01 16:48:29 +0800

    change

---
+++ project/html/4individualpage.php
@@ -0,0 +1,332 @@
+<html lang="en">
+  <head>
+    <title>🐶 FureverHome 🐱</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+    <script src="../js/main.js"></script>
+    <script src="../ownjs/8adoptionbasket.js"></script>
+  
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700, 900|Vollkorn:400i" rel="stylesheet">
+    <link rel="stylesheet" href="../fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/jquery-ui.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700, 900|Vollkorn:400i" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/jquery-ui.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="../fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="../css/aos.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+    <script src="../ownjs/4individualpage.js"></script>
+
+    <!-- Unknown stylesheet -->
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="../fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="../css/aos.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- API DOG NAME -->
+    <title>Paris</title>
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300" id="home-section" onload="load_individual_dog()"> 
+  <div class="site-wrap">
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+  <!-- Navigation Bar -->
+  <div class="site-wrap">
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+    <div class="site-mobile-menu-body"></div>
+  </div>
+  
+  <header class="site-navbar js-sticky-header site-navbar-target" role="banner" >
+
+    <div class="container">
+
+      <div class="row align-items-center">
+        <div class="col-6 col-xl-2">
+          <h1 class="mb-0 site-logo">
+            <a href="3mainpage.html" class="h2 mb-0 navbar-brand">
+              <img src="../html/images/logo_no_bg.png" height="55" alt="Furever_home_logo">
+              <span class="text-primary">FureverHome</span>
+            </a>
+          </h1>
+        </div>
+
+        <div class="col-12 col-md-10 d-none d-xl-block">
+          <nav class="site-navigation position-relative text-right" role="navigation">
+
+            <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+              <li><a href="3mainpage.html" class="nav-link">Home</a></li>
+              <li><a href= "7profilepage.html" class="nav-link">🙍Profile</a></li>
+              <li><a href= "6appointmentpage.html">📆Appointment</a></li>
+              <li><a href="8adoptionbasket.html">🛒Adoption Basket</a></li>
+            </ul>
+          </nav>          
+        </div>   
+
+        <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+
+      </div>
+    </div>
+  </header>
+        
+        
+
+  <!-- Carousel Code -->
+  <!-- <section class="site-section" id="pet-section" style="border: solid brown 1px;">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="images/golden1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="images/golden2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="images/golden3.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section> -->
+  <!-- End carousell -->
+
+  <!-- information part -->
+  <!-- Body -->
+  <section class="site-section" id="faq-section">
+    <div class="container" id="accordion">
+      <div class="row justify-content-center" data-aos="fade-up">
+        <div class="col-lg-6 text-center heading-section mb-5">
+          <div class="paws">
+            <span class="icon-paw"></span>
+          </div>
+          <h2 class="text-black mb-2"  id="dog_name">Details</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        </div>
+      </div>
+
+      <!-- content -->
+      <div class="row accordion justify-content-center block__76208">
+        <div class="col-lg-6 order-lg-2 mb-5 mb-lg-0" data-aos="fade-up"  data-aos-delay="">
+          <!-- <img src="images/golden1.jpg" alt="Image" class="img-fluid rounded"> -->
+          <!-- Carousel Code -->
+          <!-- <section class="site-section" id="pet-section" style="border: solid brown 1px;"> -->
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="border: solid brown 1px;">
+            <!-- <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol> -->
+            <div class="carousel-inner" id="dog_images">
+              <div class="carousel-item active" >
+                <img class="d-block w-100" src="images/golden1.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/golden2.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/golden3.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" id="caro_buttons_left">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" id="caro_buttons_right">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+          <!-- </section> -->
+        <!-- End carousell -->
+        </div>
+        <!-- details -->
+        <div class="col-lg-5 mr-auto order-lg-1" data-aos="fade-up"  data-aos-delay="100">
+          <div class="accordion-item">
+            <h3 class="mb-0 heading">
+              <a class="btn-block" data-toggle="collapse" href="#collapseFive" role="button" aria-expanded="true" aria-controls="collapseFive" >Dogs Details<span class="icon"></span></a>
+            </h3>
+            <div id="collapseFive" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+              <div class="body-text" id='dog_details'>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              </div>
+            </div>
+          </div> 
+
+          <div class="accordion-item">
+            <h3 class="mb-0 heading">
+              <a class="btn-block" data-toggle="collapse" href="#collapseSix" role="button" aria-expanded="false" aria-controls="collapseSix">Attributes<span class="icon"></span></a>
+            </h3>
+            <div id="collapseSix" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+              <div class="body-text" id="dog_attributes">
+                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+              </div>
+            </div>
+          </div> 
+
+          <div class="accordion-item">
+            <h3 class="mb-0 heading">
+              <a class="btn-block" data-toggle="collapse" href="#collapseSeven" role="button" aria-expanded="false" aria-controls="collapseSeven">Description<span class="icon"></span></a>
+            </h3>
+            <div id="collapseSeven" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+              <div class="body-text" id='dog_description'>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+              </div>
+            </div>
+          </div> 
+
+          <div class="accordion-item">
+            <h3 class="mb-0 heading">
+              <a class="btn-block" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseEight">Contact Details<span class="icon"></span></a>
+            </h3>
+            <div id="collapseEight" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+              <div class="body-text" id="dog_contact_details">
+                <p>Enter some random IDs</p>
+              </div>
+            </div>
+          </div> 
+        </div>
+        <!-- end details -->
+
+        
+      </div>
+    </div>
+  </section>
+  <!--  -->
+
+  <!-- Wishlist Button-->
+  <div class='row justify-content-center'> 
+  <button class="col mx-5 btn btn-primary" onclick="addPets()" name='wishlistbutton'>🛒 Add to Basket </button>
+  <br>
+  <!-- Share button-->
+  <button class="col mx-5 btn btn-primary" id='sharebutton' name='sharebutton'>📩 Share</button>
+  </div>
+  </section>
+  <hr>
+  <!-- end of information part -->
+  
+
+  <!-- Footer -->
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-5">
+              <h2 class="footer-heading mb-4">About Us</h2>
+              <p>🐶FureverHome🐱 is a one-stop Pet Adoption portal that helps you find your perfect pet! Our portal allows you to search and set appointments with the nearest pet adoption places, add interested pets into your wishlist and many more!</p>
+            </div>
+            <div class="col-md-3 ml-auto">
+              <h2 class="footer-heading mb-4">Quick Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="3mainpage.html" class="smoothscroll">Home</a></li>
+                <li><a href="7profilepage.html" class="smoothscroll">Profile</a></li>
+                <li><a href= "6appointmentpage.html" class="smoothscroll">Appointment</a></li>
+                <li><a href="8adoptionbasket.html" class="smoothscroll">Adoption Basket</a></li>
+              </ul>
+            </div>
+            <!-- Social Media Buttons-->
+            <div class="col-md-3">
+              <h2 class="footer-heading mb-4">Follow Us</h2>
+              <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+          <form action="#" method="post" class="footer-subscribe">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary text-black" type="button" id="button-addon2">Send</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row pt-5 mt-5 text-center">
+        <div class="col-md-12">
+          <div class="border-top pt-5">
+            <p class="copyright"><small>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This portal is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by FureverHome
+            </small></p> 
+          </div>
+        </div> 
+      </div>
+    </div>
+  </footer>
+
+
+    
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+  <!-- Scripts -->
+  <script src="../js/jquery-3.3.1.min.js"></script>
+  <script src="../js/jquery-ui.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.countdown.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+  <script src="../js/aos.js"></script>
+  <script src="../js/jquery.fancybox.min.js"></script>
+  <script src="../js/jquery.sticky.js"></script>
+  <script src="../js/isotope.pkgd.min.js"></script>
+  <script src="../js/main.js"></script>
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="https://unpkg.com/@petfinder/petfinder-js/dist/petfinder.min.js"></script>
+</body>
+</html>
