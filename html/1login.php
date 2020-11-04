d5914cc8a37c3d32e1f34c119ca6b64ea9626091
kingsoncjx <72540450+kingsoncjx@users.noreply.github.com>
2020-11-04 23:13:44 +0800

    Update 1login.php

--- html/1login.php
+++ html/1login.php
@@ -68,7 +68,7 @@ foreach ($accounts as $account){
       <div class="row  d-flex justify-content-center align-middle mt-4 ">
         <div class="col-6 shadow white mt-5 mb-5">
           <div class="row d-flex justify-content-center">
-              <img src="/images/logo_no_bg.png">
+              <img src="../images/logo_no_bg.png">
               <h1 class="col-12 font-weight-normal mt-3" style="margin-bottom: 15px;">Login</h1> 
               <br>
 
