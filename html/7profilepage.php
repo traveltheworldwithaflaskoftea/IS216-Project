3a9ad62bae25f35662c5b22ea69b08b206eeeb30
nrald <72239345+nrald@users.noreply.github.com>
2020-11-04 17:55:36 +0800

    Log-out button
    will logout, unset session and lead to log-in page

--- html/7profilepage.php
+++ html/7profilepage.php
@@ -104,12 +104,14 @@
                 <li>Phone Number: <?php echo $phone_number ?></li>
                 <li>Postal Code: <?php echo $postalcode ?></li>
             </ul>
-              <button type="button" class="btn btn-primary">
-                <a href="9passwordreset.php" class="text-white"> Password reset </a>
+            <button type="button" class="btn btn-primary">
+                <a href="9passwordreset.html" class="text-white"> Password reset </a>
               </button>
+              <button class="btn btn-primary" id='sharebutton' name='sharebutton'> <a href='logout.php' class="text-white">Log Out</a></button>
           </div>
         </div>
         <div class="col-md-3 col-0"></div>
+          
     </div>
 
   </section>
