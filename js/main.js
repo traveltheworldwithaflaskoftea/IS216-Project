a43a0199e5e05dfef0ca191139a8efa4a7e45ac4
traveltheworldwithaflaskoftea <jessieng2103@gmail.com>
2020-11-13 21:18:42 +0800

    API key

--- js/main.js
+++ js/main.js
@@ -93,9 +93,15 @@ function search(){
 
 document.getElementById('dog-card-deck').innerHTML = dogGif; 
 
-	var id = '7xl0fwJI98JMdZBn8vxztbMrxm7sIgf31I7wRBchOGkAOWeGnO';
-	var secret = 'pSAX1F4ihX08DiHz6uH9AGOWvzVRTZTgAQfbta0E';
-	// var search_state = '';
+	//Wes API KEY
+	// var id = '7xl0fwJI98JMdZBn8vxztbMrxm7sIgf31I7wRBchOGkAOWeGnO';
+	// var secret = 'pSAX1F4ihX08DiHz6uH9AGOWvzVRTZTgAQfbta0E';
+	//Wes 2nd API KEY
+	var id = 'KCXb8y2r6wilER4RBfAdnYwWm6bhUFXGI09J4N6X8qRk2aaAz0';
+	var secret = 'qagSVQBSizMwEx8acEJ4ojmx9XWgTmKSCb1vvkxp';
+	//Sheri API KEY
+	// var id = 'tdIL28vzdTlXMQqyJY28FYHp8GSrkhr1taoUTiTmQaIrOxFs0g';
+	// var secret = 'ZGs2r5AvICHaynezcEv7M1LfNC35Vmuq8SDX9sj6';
 
 	var client  = new petfinder.Client({apiKey: id, secret: secret});
 	var search_age = document.getElementById('age_badge').innerHTML.slice(1,-1);
