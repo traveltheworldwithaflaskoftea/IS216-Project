35ddd9063ca2d69626afcb8d19ae1a1c43a988c4
traveltheworldwithaflaskoftea <jessieng2103@gmail.com>
2020-11-13 18:40:56 +0800

    changing of directory for logo

--- html/5adoptionmessage.php
+++ html/5adoptionmessage.php
@@ -57,6 +57,20 @@
 </head>
 <body>
 
+<link href="navbar.html" rel="import" />
+<!-- Navigation Bar -->
+<div class="site-wrap">
+  <div class="site-mobile-menu site-navbar-target">
+    <div class="site-mobile-menu-header">
+      <div class="site-mobile-menu-close mt-3">
+        <span class="icon-close2 js-menu-toggle"></span>
+      </div>
+    </div>
+  <div class="site-mobile-menu-body"></div>
+</div>
+
+
+
   <!-- Body section -->  
   <section class="" id="contact-section">
       <div class="container">
